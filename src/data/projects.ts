360210eb0d2a6f143064db0a4d0bda4dd369f413
Zyrok3 <felix.reder@gmail.com>
2024-11-18 20:15:23 +0100

    feat: Created some test Projects

---
+++ src/data/projects.ts
@@ -0,0 +1,13 @@
+export const projects = [
+    {
+        name: "Project One",
+        description: "A brief description of project one.",
+        link: "https://projectone.com",
+    },
+    {
+        name: "Project Two",
+        description: "A brief description of project two.",
+        link: "https://projecttwo.com",
+    },
+    // Add more projects as needed
+];
