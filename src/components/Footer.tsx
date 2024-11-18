261f8391265af037907355a448a3ef75af9bb7a3
Zyrok3 <felix.reder@gmail.com>
2024-11-18 20:13:49 +0100

    feat: Added a simple Code to my Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
+
+const Footer = () => (
+  <footer className="bg-gray-900 text-center py-6">
+    <p className="text-gray-500 mt-4">© 2024 Felix Reder. All Rights Reserved.</p>
+  </footer>
+);
+
+export default Footer;
